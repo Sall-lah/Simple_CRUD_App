579a934fac8714c7b2153623c3b6222749f14e02
Sall-lah <ikhsan.ibnu.abdulah@gmail.com>
2026-01-03 18:07:54 +0700

    Update
    Update frontend validation script

--- src/public/js/add-task.js
+++ src/public/js/add-task.js
@@ -1,7 +1,9 @@
+// Get base url
 const CONFIG = {
     API_BASE_URL: window.location.origin 
 };
 
+// Make post request to api
 const addTask = async (taskName, taskDescription, dueDate) => {
     try {
         const tasks = await fetch(`${CONFIG.API_BASE_URL}/api/task/add`, {
@@ -18,58 +20,78 @@ const addTask = async (taskName, taskDescription, dueDate) => {
     }
 }
 
-// Tinggal error handling kalo data ada yang kurang + nampilin kalo semisal data yang dikirim udah oke
-const createTask = async() => {
-    const taskName = document.getElementById("taskName").value;
-    const taskDescription = document.getElementById("taskDescription").value;
-    const taskDueDate = document.getElementById("taskDueDate").value;
-    currentDate = new Date();
+// Get DOM Element
+const taskDescription = document.getElementById("taskDescription");
+const taskDueDate = document.getElementById("taskDueDate");
+const submitBtn = document.getElementById("submitBtn");
 
+// Call function that send request when the input is valid and submit button is clicked
+submitBtn.addEventListener("click", async() => {
+    const taskNameValue = taskName.value;
+    const taskDescriptionValue = taskDescription.value;
+    const taskDueDateValue = taskDueDate.value;
+    const currentDate = new Date();
+
+    if(taskNameValue !== '' && taskDescriptionValue !== '' && taskDueDateValue !== '' && new Date(taskDueDateValue) > currentDate) {
+        // Hide form input
+        for(i = 0; i < 4; i++) {
+            document.getElementsByClassName("form-row")[i].style.display = "none";
+        }
+
+        // Display Success message
+        const successDisplay = document.getElementById("form-success");
+        successDisplay.style.display = "flex";
+
+        // await addTask(taskName, taskDescription, taskDueDate);
+        // const data = await task.addTask(taskName, taskDescription, taskDueDate);
+        console.log({taskNameValue, taskDescriptionValue, taskDueDateValue});
+    }
+});
+
+// Name input validation
+taskName.addEventListener("input", () => {
+    const taskNameValue = taskName.value;
     const invalidNameFeedback = document.getElementById("invalid-name-feedback");
-    const taskNameInput = document.getElementById("taskName");
-    if(taskName === '') {
-        taskNameInput.style.border = "1px solid red";
+    if(taskNameValue === '') {
+        taskName.style.border = "1px solid red";
         // taskNameInput.style.outlineColor = "red";
         invalidNameFeedback.style.display = "block";
     } else {
-        taskNameInput.style.border = "none";
+        taskName.style.border = "none";
         // taskNameInput.style.outlineColor = "black";
         invalidNameFeedback.style.display = "none";
     }
-    
+})
 
+// Description input validation
+taskDescription.addEventListener("input", () => {
+    const taskDescriptionValue = taskDescription.value;
     const invalidDescriptionFeedback = document.getElementById("invalid-description-feedback");
-    const taskDescriptionInput = document.getElementById("taskDescription");
-    if(taskDescription === '') {
-        taskDescriptionInput.style.border = "1px solid red";
+    if(taskDescriptionValue === '') {
+        taskDescription.style.border = "1px solid red";
         // taskDecsriptionInput.style.outlineColor = "red";
         invalidDescriptionFeedback.style.display = "block";
     } else {
-        taskDescriptionInput.style.border = "none";
+        taskDescription.style.border = "none";
         // taskDecsriptionInput.style.outlineColor = "black";
         invalidDescriptionFeedback.style.display = "none";
     }
+})
 
+// Date input validation
+taskDueDate.addEventListener("input", () => {
+    const currentDate = new Date();
+    const taskDueDateValue = taskDueDate.value;
     const invalidDateFeedback = document.getElementById("invalid-date-feedback");
-    const taskDateInput = document.getElementById("taskDueDate");
-    if(taskDueDate === '') {
-        taskDateInput.style.border = "1px solid red"
+
+    if(taskDueDateValue === '' || new Date(taskDueDateValue) < currentDate) {
+        taskDueDate.style.border = "1px solid red"
         // taskDateInput.style.outlineColor = "red";
         invalidDateFeedback.style.display = "block";
     } else {
-        taskDateInput.style.border = "none"
+        taskDueDate.style.border = "none"
         // taskDateInput.style.outlineColor = "black";
         invalidDateFeedback.style.display = "none";
     }
+})
 
-    if(taskName == '' && taskDescription !== '' && taskDueDate !== '' && new Date(taskDueDate) < currentDate) {
-        // await addTask(taskName, taskDescription, taskDueDate);
-        // const data = await task.addTask(taskName, taskDescription, taskDueDate);
-        console.log({taskName, taskDescription, taskDueDate});
-    }
-    
-    console.log({taskName, taskDescription, taskDueDate});
-};
-
-const submitBtn = document.getElementById("submitBtn");
-submitBtn.addEventListener("click", createTask);
